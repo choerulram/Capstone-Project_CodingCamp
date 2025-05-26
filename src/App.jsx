072c2cb06f93e47b65cc8946c079d273ab7b7ba0
Choerul <achmadchoerul12345@gmail.com>
2025-05-26 15:22:42 +0700

    chore: integrate Tailwind CSS into React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl text-red-500 font-bold">Hello World</h1>
 
-export default App
+      <div className="flex justify-center items-center h-screen">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img
+          className="w-full"
+          src="https://placehold.co/600x400"
+          alt="Sunset in the mountains"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
