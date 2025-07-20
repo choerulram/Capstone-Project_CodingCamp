37faef86d99c0763b0842ab966e68463debd910a
Choerul <achmadchoerul12345@gmail.com>
2025-07-20 14:17:32 +0700

    feat(nutrition): update NutritionHeader component content

--- src/components/nutrition/NutritionHeader.jsx
+++ src/components/nutrition/NutritionHeader.jsx
@@ -4,10 +4,11 @@ const NutritionHeader = () => {
   return (
     <div className="bg-main text-white p-4 md:p-8 rounded-lg md:rounded-xl shadow-lg mb-4 md:mb-8 transition-all duration-300 hover:shadow-xl">
       <h1 className="text-2xl md:text-3xl font-bold transform transition-all duration-300 hover:scale-105">
-        Kebutuhan Gizi Harian
+        Kebutuhan Gizi Anda
       </h1>
       <p className="text-base md:text-lg mt-1 md:mt-2 text-teal-100 transition-opacity duration-300 hover:text-white">
-        Rekomendasi asupan gizi harian berdasarkan profil Anda
+        Tabel di bawah ini menampilkan rekomendasi asupan gizi harian, mingguan,
+        dan bulanan berdasarkan profil Anda.
       </p>
     </div>
   );
